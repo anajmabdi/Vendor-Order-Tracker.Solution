bfe1171b8b68b5ce776bc8765433fde39ffd2989
Najma A <110376508+anajmabdi@users.noreply.github.com>
2023-02-25 17:37:42 -0800

    Initial commit

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassName
+  {
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      //testing code will go here
+    }
+  }
+}
