f10305ea3b325597d998b8086bf4620d655f4e6c
Najma Abdi <anajmabdi@gmail.com>
2023-03-04 23:39:40 -0800

    add routes and views related to Vendors

---
+++ Tracker/Controllers/VendorController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Tracker.Models;
+
+namespace Tracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
+
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/vendors")]
+        public ActionResult Create(string vendorName, string vendorDescription)
+        {
+            Vendor newVendor = new Vendor(vendorName, vendorDescription);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor pickedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = pickedVendor.Orders;
+            model.Add("vendor", pickedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
+        }
+        [HttpPost("/vendors/{vendorId}/orders")]
+        public ActionResult Create(int vendorId, string orderTitle, int orderPrice, string orderInfo)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor foundVendor = Vendor.Find(vendorId);
+            Order newOrder = new Order(orderTitle, orderInfo, orderPrice);
+            foundVendor.AddOrder(newOrder);
+            List<Order> vendorOrders = foundVendor.Orders;
+            model.Add("orders", vendorOrders);
+            model.Add("vendor", foundVendor);
+            return View("Show", model);
+        }
+    }
+}
